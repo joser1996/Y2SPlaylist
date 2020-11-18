edc30b9e5401b5e4d3d22483d2a6178ab7bbf7ec
Makaveli <josertrs@gmail.com>
2020-11-17 16:11:35 -0800

    youtube client side

---
+++ YouTubeClient.py
@@ -0,0 +1,49 @@
+from googleapiclient.discovery import build
+import json
+import sys
+
+class YouTubeClient:
+    def __init__(self, key, playlistID):
+        self.key = key
+        self.playlistID = playlistID
+        self.youtube = build('youtube', 'v3', developerKey=key)
+        self.playlist = []
+        self.songs = []
+
+    def getPlaylistItems(self):
+        try:
+
+            nextPageToken = None
+            while True:
+                request = self.youtube.playlistItems().list(
+                    part='contentDetails, snippet',
+                    playlistId=self.playlistID,
+                    maxResults=50,
+                    pageToken=nextPageToken
+                )
+                response = request.execute()
+                self.playlist = response
+
+                # get playlist songs
+                self.getPlaylistSongs()
+
+                nextPageToken = response.get('nextPageToken')
+                if not nextPageToken:
+                    break
+        except:
+            e = sys.exc_info()[0]
+            print("Oops something went wrong")
+            print(e)
+
+    def getPlaylistSongs(self):
+        items = self.playlist["items"]
+        for item in items:
+            songTitle = item["snippet"]["title"]
+            self.songs.append(songTitle)
+
+    def printPlaylistItems(self):
+        for song in self.songs:
+            print(song)
+
+    def printPlaylist(self):
+        print(json.dumps(self.playlist, indent=2))
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-#First goal is to connect to youtube and get a list of videos in said playlist
-#   - API to connect to youtube via python
-#   - API to retrive playlist data
+from YouTubeClient import YouTubeClient
+import os
+
+
+def main():
+    PL_ID = 'PLLsUFwLen8AoiE-DRsusGl5mwr6ysteGR'
+    api_key = os.environ.get('API_KEY')
+    youtube = YouTubeClient(api_key, PL_ID)
+
+    #get youtube playlist and print songs in playlist
+    youtube.getPlaylistItems()
+    youtube.printPlaylistItems()
+
+if __name__ == "__main__":
+    main()
